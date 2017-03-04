594598cfff8604c45257025780ae223418639c23
pardeep kumar <pardeep kumar>
2017-03-04 12:11:52 +0500

    third update

---
+++ javascript/main1.js
@@ -0,0 +1,29 @@
+ // document.write("hello world");
+ // var printhello=12;
+ // var printhello1="hello";
+
+// var input=[1,2,3,4];
+// document.write(input[3],input[2],input[1],input[0]);
+// var input=1234;
+// temp=0;
+// temp=input%10;
+// document.wri
+// var num1=prompt("enter first num");
+// var num2=prompt("enter second num");
+// var n1=parseInt(num1);
+// var n2=parseInt(num2);
+// document.write(n1+n2);
+
+//we can also use this by other method function inside the function
+// var num1=parseInt(prompt("enter first num"));
+// var num2=parseInt(prompt("enter second num"));
+// document.write(num1+num2);
+
+function inputfunction(){
+  var  input=prompt("what is ur name");
+  return input;
+}
+function outputfunction()
+{
+  document.write(inputfunction);
+}
